6a29c986527dd799007c70e4250e7792da51ae9d
Quyen <doquyendiep2410@gmail.com>
2025-03-03 15:25:08 +0700

    Push_ATM

--- src/main/java/com/fptaptech/atmsys/config/SecurityConfig.java
+++ src/main/java/com/fptaptech/atmsys/config/SecurityConfig.java
@@ -2,14 +2,12 @@ package com.fptaptech.atmsys.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Configuration //Đánh dấu đây là class cấu hình
 @EnableWebSecurity //Bật tính năng bảo mật
 public class SecurityConfig {
